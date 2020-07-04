af12dc6f716386348562a22907d3fbcc07c5dd14
mrtitanic6 <67525384+mrtitanic6@users.noreply.github.com>
2020-07-03 22:02:36 -0500

    Create module1.py

---
+++ module1.py
@@ -0,0 +1,30 @@
+name = "cat"
+print(name + name + str(10))
+
+number1=11
+number2=4
+print(number1**5 + number2**5)
+
+print("hello " + name.upper())
+
+color = "blue"         #this is a variable for colors
+print(color.upper())
+print(color.lower())
+print(color.title())
+
+firstname = "Daniel"
+lastname = "McHenry"
+full_name = firstname + " " + lastname
+print("My name is " + full_name)
+
+age = 150
+message = "Happy " + str(age) + "th Birthdy"
+print(message)
+
+dogmultpler = 7
+humanage = dogmultpler * age
+print("you said you are " + str(age) + " years old, that means you are " + str(humanage) + " in dog years")
+
+cheese, ham, bread = 2, 1, 10
+print(" i want " + str(cheese) + " cheeses and " + str(bread) + " breads ")
+ 
